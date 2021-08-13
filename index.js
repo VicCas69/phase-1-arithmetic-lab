6ad6f5f24a344099dccce4798d606fa1aa29cddf
victor castro <victor_castro_01@hotmail.com>
2021-08-13 18:27:58 -0400

    commit tree 6eaa728e95fb19028c0992a3f40440f73c33afad

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 =2;
+const num2 = 31;
 
-// Write your code here
+const multiply = num1 * num2;
+
+let random;
+random= Math.floor (Math.random() + 1);
+
+const num3 = 16;
+const num4 = 6;
+let mod;
+mod = num3 % num4;
+
+let max;
+max = Math.max(5, 10, 15, 20);
